9c0d9bb59d62e3d21492f8f1d0694cac3be78e24
saba-bar95 <s.barbakadze95@gmail.com>
2025-07-18 17:45:00 +0400

    add business histogram component

--- src/components/Context/Contexts/BusinessStatistics/BusinessHistogram/BusinessHistogram.jsx
+++ src/components/Context/Contexts/BusinessStatistics/BusinessHistogram/BusinessHistogram.jsx
@@ -1,10 +1,53 @@
-import "./BusinessHistogram.scss";
+import YearsSelect from "../YearsSelect";
+import IndicatorsSelect from "../IndicatorsSelect";
+import RegionSelect from "../RegionSelect";
+import { useContext } from "react";
+import { QueriesContext } from "../../../../../App";
+import { useParams } from "react-router";
 
 const BusinessHistogram = () => {
+  const {
+    selectedQuery,
+    indicator,
+    indicatorInfo,
+    indicatorYear,
+    yearlyRegionData,
+  } = useContext(QueriesContext);
+
+  const { language } = useParams();
+
   return (
-    <>
-      <h1>Business histogram</h1>
-    </>
+    <div className="business-indicator">
+      <div
+        className="container"
+        style={{ position: "relative", width: "100%" }}>
+        <IndicatorsSelect />
+      </div>
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          gap: "20px",
+          width: "100%",
+          justifyContent: "space-between",
+        }}>
+        <RegionSelect />
+        <YearsSelect />
+      </div>
+      <div className="diagram-header">
+        <p className="diagram-p">
+          {selectedQuery[`title_${language}`]}: {indicator}
+          {indicatorInfo[`measurement_${language}`] && (
+            <> ({indicatorInfo[`measurement_${language}`]})</>
+          )}
+        </p>
+
+        <p className="diagram-p">
+          {language === "en" ? "Year" : "წელი"}{" "}
+          <span className="diagram-span">{indicatorYear}</span>
+        </p>
+      </div>
+    </div>
   );
 };
 
