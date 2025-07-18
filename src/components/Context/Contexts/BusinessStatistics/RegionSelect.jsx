34038ba9f0ad334aa714a0413d9c54df98ba4756
saba-bar95 <s.barbakadze95@gmail.com>
2025-07-18 17:45:27 +0400

    add region select

---
+++ src/components/Context/Contexts/BusinessStatistics/RegionSelect.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { useParams } from "react-router";
+import { QueriesContext } from "../../../../App";
+
+const RegionSelect = () => {
+  const { language } = useParams();
+  const { regData, selectedRegionID, setSelectedRegionID } =
+    useContext(QueriesContext);
+
+  const handleRegionChange = (e) => {
+    setSelectedRegionID(e.target.value);
+  };
+
+  return (
+    <select
+      name="regionSelect"
+      id="region"
+      value={selectedRegionID ?? ""}
+      onChange={handleRegionChange}>
+      {regData
+        .filter((reg) => reg.region_id !== "12" && reg.region_id !== "48")
+        .sort((a, b) => Number(a.region_id) - Number(b.region_id)) // 🔽 Descending sort
+        .map((reg) => (
+          <option key={reg.region_id} value={reg.region_id}>
+            {reg[`name_${language}`]}
+          </option>
+        ))}
+    </select>
+  );
+};
+
+export default RegionSelect;
