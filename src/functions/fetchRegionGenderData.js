2dec3420f7f25f763e2708b26c3dc734f367435d
saba-bar95 <s.barbakadze95@gmail.com>
2025-07-21 16:42:12 +0400

    add fetchin region gender data

---
+++ src/functions/fetchRegionGenderData.js
@@ -0,0 +1,21 @@
+import backEndUrl from "../BackEndUrl";
+
+const fetchRegionGenderData = async (indicator, regionID, year) => {
+  try {
+    const response = await fetch(
+      `${backEndUrl}/api/getReg${indicator}/${regionID}?fyear=${year}&myear=${year}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    return data; // Return the fetched data
+  } catch (error) {
+    console.log(error.message);
+    return null; // Return null or handle the error as needed
+  }
+};
+
+export default fetchRegionGenderData;
