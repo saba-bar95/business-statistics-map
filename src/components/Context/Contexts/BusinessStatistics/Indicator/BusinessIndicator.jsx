bc58336d773df9a16423ac75ff52208021e93581
saba-bar95 <saba.barb95@gmail.com>
2025-11-19 23:31:29 +0400

    fix some bugs

--- src/components/Context/Contexts/BusinessStatistics/Indicator/BusinessIndicator.jsx
+++ src/components/Context/Contexts/BusinessStatistics/Indicator/BusinessIndicator.jsx
@@ -32,17 +32,21 @@ const BusinessIndicator = () => {
   const isByGender =
     indicator === indicators[12] || indicator === indicators[11];
 
-  const sortedByGender = regData
-    .filter(
-      (region) =>
-        region[`f_${indicatorYear}`] !== 0 &&
-        region[`f_${indicatorYear}`] !== null
-    )
-    .sort((a, b) => b[`f_${indicatorYear}`] - a[`f_${indicatorYear}`]);
+  const sortedByGender =
+    regData &&
+    regData
+      .filter(
+        (region) =>
+          region[`f_${indicatorYear}`] !== 0 &&
+          region[`f_${indicatorYear}`] !== null
+      )
+      .sort((a, b) => b[`f_${indicatorYear}`] - a[`f_${indicatorYear}`]);
 
-  const sortedRegions = regData
-    .filter((region) => region[`w_${indicatorYear}`] !== null) // Exclude regions with null values
-    .sort((a, b) => b[`w_${indicatorYear}`] - a[`w_${indicatorYear}`]); // Sort in descending order
+  const sortedRegions =
+    regData &&
+    regData
+      .filter((region) => region[`w_${indicatorYear}`] !== null) // Exclude regions with null values
+      .sort((a, b) => b[`w_${indicatorYear}`] - a[`w_${indicatorYear}`]); // Sort in descending order
 
   return (
     <div className="business-indicator">
@@ -99,93 +103,106 @@ const BusinessIndicator = () => {
             <p>{indicatorInfo && indicatorInfo[`measurement_${language}`]}</p>
           </div>
           <div className="regions">
-            {sortedRegions.map((region) => {
-              const regId = region.region_id;
-              if (munData) {
-                const muns = munData.filter((mun) => {
-                  const munId = mun.municipal_ ? mun.municipal_.slice(0, 2) : 0;
-                  return munId === regId;
-                });
-                sortedMuns = muns
-                  .filter((mun) => mun[`w_${indicatorYear}`] !== null)
-                  .sort(
-                    (a, b) => b[`w_${indicatorYear}`] - a[`w_${indicatorYear}`]
-                  );
-              }
+            {sortedRegions &&
+              sortedRegions.map((region) => {
+                const regId = region.region_id;
+                if (munData) {
+                  const muns =
+                    munData &&
+                    munData.filter((mun) => {
+                      const munId = mun.municipal_
+                        ? mun.municipal_.slice(0, 2)
+                        : 0;
+                      return munId === regId;
+                    });
+                  sortedMuns =
+                    muns &&
+                    muns
+                      .filter((mun) => mun[`w_${indicatorYear}`] !== null)
+                      .sort(
+                        (a, b) =>
+                          b[`w_${indicatorYear}`] - a[`w_${indicatorYear}`]
+                      );
+                }
 
-              return (
-                <div className="wrapper" key={regId}>
-                  <div
-                    className={"paras"} // Add conditional class here
-                    onClick={() => {
-                      if (regId === selectedRegionId) setSelectedRegionId(null);
-                      else setSelectedRegionId(regId); // Set the selected region ID
-                      setIsMunOpen((prev) => (prev === regId ? null : regId));
-                    }}
-                    style={
-                      indicatorYear > 2013 && regId === "11"
-                        ? { marginLeft: "23px" }
-                        : undefined
-                    }>
-                    {region[`w_${indicatorYear}`] > 0 && (
-                      <>
-                        <p>
-                          {regId === "11" ? null : selectedRegionId ===
-                            regId ? (
-                            <>{indicatorYear > 2013 && <AngleDown />}</>
-                          ) : (
-                            <>{indicatorYear > 2013 && <AngleRight />}</>
-                          )}
-                          {region[`name_${language}`]}
-                        </p>
-                        <p>
-                          {typeof region[`w_${indicatorYear}`] === "number"
-                            ? region[`w_${indicatorYear}`].toFixed(1)
-                            : region[`w_${indicatorYear}`]}
-                        </p>
-                      </>
-                    )}
-                  </div>
-                  {isMunOpen &&
-                    indicatorYear > 2013 &&
-                    selectedRegionId === regId &&
-                    regId !== "11" && (
-                      <div className="mun-container">
-                        {sortedMuns &&
-                          sortedMuns
-                            .sort((a, b) => b.value - a.value)
-                            .map((mun) => {
-                              const name = municipalities.features
-                                .filter(
-                                  (mun1) =>
-                                    mun1.properties.MUNICIPAL1 ===
-                                    mun.municipal_
-                                )
-                                .map((mun2) =>
-                                  language === "en"
-                                    ? mun2.properties.NAME_EN
-                                    : mun2.properties.NAME_SYLFA
+                return (
+                  <div className="wrapper" key={regId}>
+                    <div
+                      className={"paras"} // Add conditional class here
+                      onClick={() => {
+                        if (regId === selectedRegionId)
+                          setSelectedRegionId(null);
+                        else setSelectedRegionId(regId); // Set the selected region ID
+                        setIsMunOpen((prev) => (prev === regId ? null : regId));
+                      }}
+                      style={
+                        indicatorYear > 2013 && regId === "11"
+                          ? { marginLeft: "23px" }
+                          : undefined
+                      }
+                    >
+                      {region[`w_${indicatorYear}`] > 0 && (
+                        <>
+                          <p>
+                            {regId === "11" ? null : selectedRegionId ===
+                              regId ? (
+                              <>{indicatorYear > 2013 && <AngleDown />}</>
+                            ) : (
+                              <>{indicatorYear > 2013 && <AngleRight />}</>
+                            )}
+                            {region[`name_${language}`]}
+                          </p>
+                          <p>
+                            {typeof region[`w_${indicatorYear}`] === "number"
+                              ? region[`w_${indicatorYear}`].toFixed(1)
+                              : region[`w_${indicatorYear}`]}
+                          </p>
+                        </>
+                      )}
+                    </div>
+                    {isMunOpen &&
+                      indicatorYear > 2013 &&
+                      selectedRegionId === regId &&
+                      regId !== "11" && (
+                        <div className="mun-container">
+                          {sortedMuns &&
+                            sortedMuns
+                              .sort((a, b) => b.value - a.value)
+                              .map((mun) => {
+                                const name = municipalities.features
+                                  .filter(
+                                    (mun1) =>
+                                      mun1.properties.MUNICIPAL1 ===
+                                      mun.municipal_
+                                  )
+                                  .map((mun2) =>
+                                    language === "en"
+                                      ? mun2.properties.NAME_EN
+                                      : mun2.properties.NAME_SYLFA
+                                  );
+
+                                return (
+                                  <div
+                                    className="mun-paras"
+                                    key={mun.municipal_}
+                                  >
+                                    <p>{name}</p>
+                                    <p>
+                                      {typeof mun[`w_${indicatorYear}`] ===
+                                      "number"
+                                        ? mun[`w_${indicatorYear}`].toFixed(1)
+                                        : typeof mun.value === "number"
+                                        ? mun.value.toFixed(1)
+                                        : "N/A"}
+                                    </p>
+                                  </div>
                                 );
-
-                              return (
-                                <div className="mun-paras" key={mun.municipal_}>
-                                  <p>{name}</p>
-                                  <p>
-                                    {typeof mun[`w_${indicatorYear}`] ===
-                                    "number"
-                                      ? mun[`w_${indicatorYear}`].toFixed(1)
-                                      : typeof mun.value === "number"
-                                      ? mun.value.toFixed(1)
-                                      : "N/A"}
-                                  </p>
-                                </div>
-                              );
-                            })}
-                      </div>
-                    )}
-                </div>
-              );
-            })}
+                              })}
+                        </div>
+                      )}
+                  </div>
+                );
+              })}
           </div>
         </div>
       )}
